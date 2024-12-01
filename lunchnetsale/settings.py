37b4206c751dc18f68eb85b2cc0b36531ec7cb6c
shohei tanaka <shoheitanaka@shoheinoMacBook-Pro.local>
2024-12-01 19:17:23 +0900

    Fix STATIC_ROOT for Heroku

--- lunchnetsale/settings.py
+++ lunchnetsale/settings.py
@@ -136,6 +136,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+# settings.py に追加
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
