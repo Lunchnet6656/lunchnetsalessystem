1877f5d2c6765497dc14aad056ad6b06668ec7c0
shohei tanaka <shoheitanaka@shoheinoMacBook-Pro.local>
2024-12-25 15:36:18 +0900

    PostgreSQL

--- lunchnetsale/settings.py
+++ lunchnetsale/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 import os
 
 from datetime import timedelta
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -95,11 +96,9 @@ WSGI_APPLICATION = 'lunchnetsale.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
+
 
 LOGIN_URL = '/login/'  # 未ログインのユーザーがアクセスしようとしたときにリダイレクトされるログインページのURL名
 LOGOUT_REDIRECT_URL = '/login/'  # ログアウト後にリダイレクトされるURL名
